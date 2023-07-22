985261d69e9a6ada3a102a8552895cb4d4fa5d8c
James Pond <james@cipher.host>
2023-07-22 14:21:55 -0300

    Refactor entire module
    Signed-off-by: James Pond <james@cipher.host>

--- image.go
+++ image.go
@@ -2,7 +2,8 @@ package imgdiet
 
 import (
 	"fmt"
-	"os"
+	"io"
+	"math"
 
 	"git.sr.ht/~jamesponddotco/xstd-go/xerrors"
 	"github.com/davidbyttow/govips/v2/vips"
@@ -10,6 +11,7 @@ import (
 
 const (
 	ErrOpenImage               xerrors.Error = "failed to open image"
+	ErrNilImage                xerrors.Error = "image is nil"
 	ErrInvalidResizeDimensions xerrors.Error = "dimensions must be greater than 0"
 )
 
@@ -17,19 +19,19 @@ const (
 type Options struct {
 	// Quality defines the quality of the output image. It is a number between 0
 	// and 100.
-	Quality int
+	Quality uint
 
 	// Compression defines the compression level of the output image. It is a
 	// number between 0 and 9.
 	//
 	// Only valid for PNG images.
-	Compression int
+	Compression uint
 
 	// QuantTable defines the quantization table to be used for the output
 	// image. It is a number between 0 and 8.
 	//
 	// Only valid for JPEG images.
-	QuantTable int
+	QuantTable uint
 
 	// OptimizeCoding defines whether the output image should have its coding
 	// optimized.
@@ -71,10 +73,10 @@ type Options struct {
 func DefaultOptions() *Options {
 	return &Options{
 		Quality:            60,
-		Compression:        6,
+		Compression:        9,
 		QuantTable:         3,
 		OptimizeCoding:     true,
-		Interlaced:         true,
+		Interlaced:         false,
 		StripMetadata:      true,
 		OptimizeICCProfile: true,
 		TrellisQuant:       true,
@@ -85,26 +87,26 @@ func DefaultOptions() *Options {
 
 // Image defines an image to be optimized and manages its lifecycle.
 type Image struct {
-	// Reference is a govips.ImageRef that contains the image data.
-	Reference *vips.ImageRef
+	// reference is a govips.ImageRef that contains the image data.
+	reference *vips.ImageRef
 
-	// Options defines the parameters for image optimization.
-	Options *Options
+	// format is a string representation of the image type.
+	format string
 
-	// Type is a string representation of the image type.
-	Type string
+	// size is the size of the image in bytes.
+	size int64
 
-	// Size is the size of the image in bytes.
-	Size int64
-
-	// Saved is the size of the image after optimization in bytes.
-	Saved int64
+	// saved is the size of the image after optimization in bytes.
+	saved int64
 }
 
-// Open takes a named image file as input for reading and returns an Image
-// instance.
-func Open(name string) (*Image, error) {
-	image, err := os.ReadFile(name)
+// Open takes an io.Reader as input for reading and returns an Image instance.
+func Open(r io.Reader) (*Image, error) {
+	if r == nil {
+		return nil, fmt.Errorf("%w: %w", ErrOpenImage, ErrNilImage)
+	}
+
+	image, err := io.ReadAll(r)
 	if err != nil {
 		return nil, fmt.Errorf("%w: %w", ErrOpenImage, err)
 	}
@@ -120,15 +122,17 @@ func Open(name string) (*Image, error) {
 	}
 
 	return &Image{
-		Reference: data,
-		Type:      imageType,
-		Size:      DetectImageSize(image),
+		reference: data,
+		format:    imageType,
+		size:      DetectImageSize(image),
 	}, nil
 }
 
 // Close releases the resources associated with the Image.
 func (i *Image) Close() {
-	i.Reference.Close()
+	if i != nil && i.reference != nil {
+		i.reference.Close()
+	}
 }
 
 // Optimize takes the given Options and optimizes the image accordingly. It
@@ -139,10 +143,8 @@ func (i *Image) Optimize(opts *Options) ([]byte, error) {
 		opts = DefaultOptions()
 	}
 
-	i.Options = opts
-
-	if i.Options.OptimizeICCProfile {
-		if err := i.Reference.OptimizeICCProfile(); err != nil {
+	if opts.OptimizeICCProfile {
+		if err := i.reference.OptimizeICCProfile(); err != nil {
 			return nil, fmt.Errorf("%w", err)
 		}
 	}
@@ -152,78 +154,116 @@ func (i *Image) Optimize(opts *Options) ([]byte, error) {
 		err   error
 	)
 
-	switch i.Type {
+	switch i.format {
 	case ImageTypeJPEG:
-		image, err = i.optimizeJPEG()
+		image, err = i.optimizeJPEG(opts)
 		if err != nil {
 			return nil, fmt.Errorf("%w", err)
 		}
 	case ImageTypePNG:
-		image, err = i.optimizePNG()
+		image, err = i.optimizePNG(opts)
 		if err != nil {
 			return nil, fmt.Errorf("%w", err)
 		}
 	default:
-		return nil, fmt.Errorf("%w: %s", ErrUnsupportedImageFormat, i.Type)
+		return nil, fmt.Errorf("%w: %s", ErrUnsupportedImageFormat, i.format)
 	}
 
-	i.Saved = DetectImageSize(image)
+	i.saved = DetectImageSize(image)
 
 	return image, nil
 }
 
 // Resize takes a set of dimensions and resizes the image to those dimensions.
-// The resulting image is optimized according to the given Options.
+// If opts is not nil, the resulting image is optimized according to the given
+// Options.
 func (i *Image) Resize(width, height uint, opts *Options) ([]byte, error) {
-	if width == 0 || height == 0 {
+	if width == 0 && height == 0 {
 		return nil, fmt.Errorf("%w", ErrInvalidResizeDimensions)
 	}
 
-	if opts == nil {
-		opts = DefaultOptions()
+	aspectRatio := float64(i.reference.Width()) / float64(i.reference.Height())
+
+	if width == 0 {
+		width = uint(math.Round(float64(height) * aspectRatio))
+	} else if height == 0 {
+		height = uint(math.Round(float64(width) / aspectRatio))
 	}
 
-	i.Options = opts
-
-	if err := i.Reference.Thumbnail(int(width), int(height), vips.InterestingCentre); err != nil {
+	if err := i.reference.Thumbnail(int(width), int(height), vips.InterestingCentre); err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
 
-	return i.Optimize(i.Options)
+	if opts != nil {
+		return i.Optimize(opts)
+	}
+
+	image, _, err := i.reference.ExportNative()
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	return image, nil
 }
 
-func (i *Image) optimizeJPEG() ([]byte, error) {
+// Size returns the size of the image in bytes.
+func (i *Image) Size() int64 {
+	return i.size
+}
+
+// Saved returns the size of the image after optimization in bytes.
+func (i *Image) Saved() int64 {
+	return i.saved
+}
+
+// Width returns the width of the image in pixels.
+func (i *Image) Width() int {
+	return i.reference.Width()
+}
+
+// Height returns the height of the image in pixels.
+func (i *Image) Height() int {
+	return i.reference.Height()
+}
+
+// optimizeJPEG takes the given Options and optimizes the image accordingly. It
+// returns the optimized image as a byte slice or an error if the optimization
+// fails.
+func (i *Image) optimizeJPEG(opts *Options) ([]byte, error) {
 	options := &vips.JpegExportParams{
-		StripMetadata:      i.Options.StripMetadata,
-		Quality:            i.Options.Quality,
-		Interlace:          i.Options.Interlaced,
-		OptimizeCoding:     i.Options.OptimizeCoding,
-		TrellisQuant:       i.Options.TrellisQuant,
-		OvershootDeringing: i.Options.OvershootDeringing,
-		OptimizeScans:      i.Options.OptimizeScans,
-		QuantTable:         i.Options.QuantTable,
+		StripMetadata:      opts.StripMetadata,
+		Quality:            int(opts.Quality),
+		Interlace:          opts.Interlaced,
+		OptimizeCoding:     opts.OptimizeCoding,
+		TrellisQuant:       opts.TrellisQuant,
+		OvershootDeringing: opts.OvershootDeringing,
+		OptimizeScans:      opts.OptimizeScans,
+		QuantTable:         int(opts.QuantTable),
 	}
 
-	jpg, _, err := i.Reference.ExportJpeg(options)
+	image, _, err := i.reference.ExportJpeg(options)
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
 
-	return jpg, nil
+	return image, nil
 }
 
-func (i *Image) optimizePNG() ([]byte, error) {
+// optimizePNG takes the given Options and optimizes the image accordingly. It
+// returns the optimized image as a byte slice or an error if the optimization
+// fails.
+func (i *Image) optimizePNG(opts *Options) ([]byte, error) {
 	options := &vips.PngExportParams{
-		StripMetadata: i.Options.StripMetadata,
-		Compression:   i.Options.Compression,
-		Interlace:     i.Options.Interlaced,
-		Quality:       i.Options.Quality,
+		StripMetadata: opts.StripMetadata,
+		Compression:   int(opts.Compression),
+		Interlace:     opts.Interlaced,
+		Quality:       int(opts.Quality),
 	}
 
-	png, _, err := i.Reference.ExportPng(options)
+	image, _, err := i.reference.ExportPng(options)
 	if err != nil {
 		return nil, fmt.Errorf("%w", err)
 	}
 
-	return png, nil
+	return image, nil
 }
